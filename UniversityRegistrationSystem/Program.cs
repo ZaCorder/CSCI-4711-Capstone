6db4c8adf759998fd1f70f17a8fe68ad7ec84c67
Jonathan Delaigle <JDELAIG1@augusta.edu>
2016-11-11 17:23:01 -0500

    Adding inital DBConnector code.

--- UniversityRegistrationSystem/Program.cs
+++ UniversityRegistrationSystem/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UniversityRegistrationSystem.Control;
+
 
 namespace UniversityRegistrationSystem
 {
@@ -14,6 +16,7 @@ namespace UniversityRegistrationSystem
         [STAThread]
         static void Main()
         {
+            DBConnect db = DBConnect.Instance;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
