210584a46ecb16b50a3e449e31227ed59519bb66
Jonathan Delaigle <JDELAIG1@augusta.edu>
2016-11-13 10:06:27 -0500

    Adding docblocks.

--- UniversityRegistrationSystem/Control/DBConnect.cs
+++ UniversityRegistrationSystem/Control/DBConnect.cs
@@ -9,6 +9,9 @@ using System.Data.Common;
 
 namespace UniversityRegistrationSystem.Control
 {
+    /// <summary>
+    /// Database connector class. Used to communication with the databse.
+    /// </summary>
     class DBConnect
     {
         private SQLiteConnection dbConnection;
@@ -25,16 +28,30 @@ namespace UniversityRegistrationSystem.Control
             }
         }
 
+        /// <summary>
+        /// Use DBConnect.GetInstance().
+        /// 
+        /// The constructor for DBConnect. Is private since we are using it as
+        /// a singleton.
+        /// </summary>
         private DBConnect()
         {
             this.dbConnection = new SQLiteConnection("Data Source=db.sqlite;Version=3;");
         }
 
+        /// <summary>
+        /// Retrieve the current database connection object.
+        /// </summary>
+        /// <returns>The current database connection object.</returns>
         public SQLiteConnection GetConnection()
         {
             return this.dbConnection;
         }
 
+        /// <summary>
+        /// Save an "Account" entity to the database.
+        /// </summary>
+        /// <param name="account">The "Account" entity to be saved to the database.</param>
         public void SaveAccount(Account account)
         {
             this.dbConnection.Open();
@@ -48,6 +65,10 @@ namespace UniversityRegistrationSystem.Control
             this.dbConnection.Close();
         }
 
+        /// <summary>
+        /// Save a "Student" entity to the database.
+        /// </summary>
+        /// <param name="student">A "Student" entity to be saved to the database.</param>
         public void SaveAccount(StudentAccount student)
         {
             this.SaveAccount((Account) student);
@@ -60,6 +81,11 @@ namespace UniversityRegistrationSystem.Control
                 
         }
 
+        /// <summary>
+        /// Save a "AccountClass" relationship to the database.
+        /// </summary>
+        /// <param name="student">The "Student" entity for which to save the relationship.</param>
+        /// <param name="classRecord">The "Class" entity for which to save the relationship.</param>
         public void SaveAccountClass(StudentAccount student, Class classRecord)
         {
             this.dbConnection.Open();
@@ -72,51 +98,10 @@ namespace UniversityRegistrationSystem.Control
             this.dbConnection.Close();
         }
 
-        private void CreateTestClasses()
-        {
-            Class classA = new Class();
-            classA.CourseNo = "ENG 1101";
-            classA.Section = "N";
-            classA.ClassName = "College Composition I";
-            classA.Credits = 4;
-            classA.Location = "UH 100";
-            classA.Instructor = "Wayne";
-            classA.TimeStart = new DateTime(2016, 1, 1, 9, 0, 0);
-            classA.TimeEnd = new DateTime(2016, 1, 1, 10, 45, 0);
-            classA.StartDate =  DateTime.Now;
-            classA.EndDate = DateTime.Now.AddDays(84);
-            classA.ClassDays = "MW";
-            this.SaveClass(classA);
-
-            Class classB = new Class();
-            classB.CourseNo = "CSCI 1101";
-            classB.Section = "B";
-            classB.ClassName = "Math Structures";
-            classB.Credits = 3;
-            classB.Location = "AH 300";
-            classB.Instructor = "Grayson";
-            classB.TimeStart = new DateTime(2016, 1, 1, 11, 0, 0);
-            classB.TimeEnd = new DateTime(2016, 1, 1, 12, 45, 0);
-            classB.StartDate = DateTime.Now;
-            classB.EndDate = DateTime.Now.AddDays(84);
-            classB.ClassDays = "TR";
-            this.SaveClass(classB);
-
-            Class classC = new Class();
-            classC.CourseNo = "MATH 2011";
-            classC.Section = "D";
-            classC.ClassName = "Calculus";
-            classC.Credits = 4;
-            classC.Location = "AH 200";
-            classC.Instructor = "Pennyworth";
-            classC.TimeStart = new DateTime(2016, 1, 1, 2, 0, 0);
-            classC.TimeEnd = new DateTime(2016, 1, 1, 3, 45, 0);
-            classC.StartDate = DateTime.Now;
-            classC.EndDate = DateTime.Now.AddDays(84);
-            classC.ClassDays = "TR";
-            this.SaveClass(classC);
-        }
-
+        /// <summary>
+        /// Save a "Class" relationship to the database.
+        /// </summary>
+        /// <param name="classRecord">The "Class" entity for which to save to the database.</param>
         public void SaveClass(Class classRecord)
         {
             this.dbConnection.Open();
@@ -140,6 +125,10 @@ namespace UniversityRegistrationSystem.Control
             this.dbConnection.Close();
         }
 
+        /// <summary>
+        /// Retrieve all classes in the Database.
+        /// </summary>
+        /// <returns>An array of classes that exist.</returns>
         public Class[] GetClasses()
         {
             Class[] classes = new Class[1];
