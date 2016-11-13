8f48daaf4ecff5a128c88508fb9cb377db920757
Jonathan Delaigle <JDELAIG1@augusta.edu>
2016-11-13 11:53:20 -0500

    Finishing up the DB connector.

--- UniversityRegistrationSystem/Control/DBConnect.cs
+++ UniversityRegistrationSystem/Control/DBConnect.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data.SQLite;
 using UniversityRegistrationSystem.Entity;
 using System.Data.Common;
+using System.Data;
 
 namespace UniversityRegistrationSystem.Control
 {
@@ -48,6 +49,37 @@ namespace UniversityRegistrationSystem.Control
             return this.dbConnection;
         }
 
+        public Account GetAccount(string email, string password)
+        {
+            this.dbConnection.Open();
+            Account account = new Account();
+            string query = @"SELECT email, password, type FROM Account WHERE
+                email = @email AND password = @password";
+            SQLiteCommand command = new SQLiteCommand(query, this.dbConnection);
+            command.Parameters.AddWithValue("@email", email);
+            command.Parameters.AddWithValue("@password", password);
+            SQLiteDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+                string type = reader.GetString(2);
+                if (type == "Administrator")
+                    account = new AdministratorAccount(reader);
+                else if (type == "Student")
+                {
+                    StudentAccount studentAccount = new StudentAccount(reader);
+                    studentAccount.Classes = this.GetClasses(studentAccount);
+                    account = studentAccount;
+                }
+                else
+                    account = new Account(reader);
+                    
+            }
+            reader.Close();
+            this.dbConnection.Close();
+            return account;
+        }
+
         /// <summary>
         /// Save an "Account" entity to the database.
         /// </summary>
@@ -73,11 +105,8 @@ namespace UniversityRegistrationSystem.Control
         {
             this.SaveAccount((Account) student);
 
-            Console.WriteLine(student.Classes.Length);
-            for (int i = 0; i < student.Classes.Length; i++) {
-                if (student.Classes[i] != null)
-                 this.SaveAccountClass(student, student.Classes[i]);
-            }      
+            foreach (Class classRecord in student.Classes)
+                this.SaveAccountClass(student, classRecord);  
         }
 
         /// <summary>
@@ -95,6 +124,42 @@ namespace UniversityRegistrationSystem.Control
             command.Parameters.AddWithValue("@courseNo", classRecord.CourseNo);
             command.ExecuteNonQuery();
             this.dbConnection.Close();
+        }
+
+        /// <summary>
+        /// Create "Class" entity and write to database.
+        /// </summary>
+        /// <param name="courseNo">The course number for the class. Part of the primary key.</param>
+        /// <param name="section">The section number for the class. Part of the primary key.</param>
+        /// <param name="className">The name of the class.</param>
+        /// <param name="credits">The number of credit hours.</param>
+        /// <param name="location">The location where the class will be held.</param>
+        /// <param name="instructor">The instructor who will be teaching the class.</param>
+        /// <param name="timeStart">The time the class will start.</param>
+        /// <param name="timeEnd">The time the class will end.</param>
+        /// <param name="startDate">The date when the class will start.</param>
+        /// <param name="endDate">Then date when the class will end.</param>
+        /// <param name="classDays">The days of the week the class meets.</param>
+        /// <returns>A "Class" entity object.</returns>
+        public Class CreateClass(string courseNo, string section, string className,
+            int credits, string location, string instructor, DateTime timeStart,
+            DateTime timeEnd, DateTime startDate, DateTime endDate, string classDays)
+        {
+            Class classRecord = new Class();
+            classRecord.CourseNo = courseNo;
+            classRecord.Section = section;
+            classRecord.ClassName = className;
+            classRecord.Credits = credits;
+            classRecord.Location = location;
+            classRecord.Instructor = instructor;
+            classRecord.TimeStart = timeStart;
+            classRecord.TimeEnd = timeEnd;
+            classRecord.StartDate = startDate;
+            classRecord.EndDate = endDate;
+            classRecord.ClassDays = classDays;
+            this.SaveClass(classRecord);
+
+            return classRecord;
         }
 
         /// <summary>
@@ -128,9 +193,9 @@ namespace UniversityRegistrationSystem.Control
         /// Retrieve all classes in the Database.
         /// </summary>
         /// <returns>An array of classes that exist.</returns>
-        public Class[] GetClasses()
+        public List<Class> GetClasses()
         {
-            Class[] classes = new Class[1];
+            List<Class> classes = new List<Class>();
 
             this.dbConnection.Open();
             string query = @"SELECT * FROM Class";
@@ -138,13 +203,119 @@ namespace UniversityRegistrationSystem.Control
             SQLiteDataReader reader = command.ExecuteReader();
             if (reader.HasRows)
             {
-                classes = new Class[reader.FieldCount];
                 while (reader.Read())
-                    classes[reader.StepCount - 1] = new Class(reader);
+                {
+                    Class classRecord = new Class(reader);
+                    classes.Add(classRecord);
+                }
             }
             reader.Close();
             this.dbConnection.Close();
             return classes;
+        }
+
+        /// <summary>
+        /// Retrieve all classes for which the specified student is registerd.
+        /// </summary>
+        /// <param name="student">The "Student" entity for which to retrieve registered classes.</param>
+        /// <returns>A list of "Class" entities for which the "Student" is registerd.</returns>
+        public List<Class> GetClasses(StudentAccount student)
+        {
+            List<Class> classes = new List<Class>();
+
+            // This is pretty silly, but it is so far the only one I ran into
+            // this particlar problem as most places would not already have an
+            // opened connection. If we see this pop-up again we'll want to move
+            // it out into a connection toggle that stacks the counts of opens
+            // vs closes.
+            bool openedConnection = false;
+            if (this.dbConnection.State == ConnectionState.Closed)
+            {
+                this.dbConnection.Open();
+                openedConnection = true;
+            }
+
+            string query = @"SELECT c.* FROM AccountClass ac INNER JOIN Class c ON ac.courseNo = c.courseNo WHERE email = @email";
+            SQLiteCommand command = new SQLiteCommand(query, this.dbConnection);
+            command.Parameters.AddWithValue("@email", student.Email);
+            SQLiteDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read()) {
+                    Class classRecord = new Class(reader);
+                    classes.Add(classRecord);
+                }
+                
+            }
+            reader.Close();
+            if (openedConnection)
+                this.dbConnection.Close();
+            return classes;
+        }
+
+        /// <summary>
+        /// Determines if a Class already exists.
+        /// </summary>
+        /// <param name="courseNo"></param>
+        /// <param name="section"></param>
+        /// <returns>True if the class exists and False otherwise.</returns>
+        public bool DoesClassExist(string courseNo, string section)
+        {
+            this.dbConnection.Open();
+            string query = @"SELECT COUNT(1) FROM Class
+                WHERE courseNo = @courseNo AND section = @section LIMIT 1";
+            SQLiteCommand command = new SQLiteCommand(query, this.dbConnection);
+            command.Parameters.AddWithValue("@courseNo", courseNo);
+            command.Parameters.AddWithValue("@section", section);
+            bool classExists = Int32.Parse(command.ExecuteScalar().ToString()) > 0;
+            this.dbConnection.Close();
+            return classExists;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="courseNo"></param>
+        /// <param name="section"></param>
+        /// <returns></returns>
+        public Class GetClass(string courseNo, string section)
+        {
+            this.dbConnection.Open();
+            Class classRecord = new Class();
+            string query = @"SELECT * FROM Class WHERE
+                courseNo = @courseNo AND section = @section";
+            SQLiteCommand command = new SQLiteCommand(query, this.dbConnection);
+            command.Parameters.AddWithValue("@courseNo", courseNo);
+            command.Parameters.AddWithValue("@section", section);
+            SQLiteDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+                classRecord = new Class(reader);
+            }
+            reader.Close();
+            this.dbConnection.Close();
+            return classRecord;
+        }
+
+        /// <summary>
+        /// Register a student to a class.
+        /// </summary>
+        /// <param name="fullCourseId">The full course id for a class. Example: courseNo-section.</param>
+        /// <param name="student">The "Student" entity for which to register a class.</param>
+        /// <returns></returns>
+        public StudentAccount Register(string fullCourseId, StudentAccount student)
+        {
+            String[] parsedCourseId = fullCourseId.Split('-');
+            if (parsedCourseId.Length != 2)
+                throw new Exception("Invalid full course id. Ex: courseNo-section");
+
+            Class classRecord = GetClass(parsedCourseId[0], parsedCourseId[1]);
+            if (classRecord != null)
+                student.Classes.Add(classRecord);
+
+            this.SaveAccount(student);
+            return student;
         }
     }
 }
--- UniversityRegistrationSystem/Control/DBConnectorInit.cs
+++ UniversityRegistrationSystem/Control/DBConnectorInit.cs
@@ -157,47 +157,20 @@ namespace UniversityRegistrationSystem.Control
         /// </summary>
         private void CreateTestClasses()
         {
-            Class classA = new Class();
-            classA.CourseNo = "ENG 1101";
-            classA.Section = "N";
-            classA.ClassName = "College Composition I";
-            classA.Credits = 4;
-            classA.Location = "UH 100";
-            classA.Instructor = "Wayne";
-            classA.TimeStart = new DateTime(2016, 1, 1, 9, 0, 0);
-            classA.TimeEnd = new DateTime(2016, 1, 1, 10, 45, 0);
-            classA.StartDate = DateTime.Now;
-            classA.EndDate = DateTime.Now.AddDays(84);
-            classA.ClassDays = "MW";
-            dbConnector.SaveClass(classA);
+            DateTime startTime = new DateTime(2016, 1, 1, 9, 0, 0);
+            DateTime endTime = new DateTime(2016, 1, 1, 10, 45, 0);
+            dbConnector.CreateClass("ENG 1101", "N", "College Composition I", 4,
+                "UH 100", "Wayne", startTime, endTime, DateTime.Now, DateTime.Now.AddDays(84), "MW");
 
-            Class classB = new Class();
-            classB.CourseNo = "CSCI 1101";
-            classB.Section = "B";
-            classB.ClassName = "Math Structures";
-            classB.Credits = 3;
-            classB.Location = "AH 300";
-            classB.Instructor = "Grayson";
-            classB.TimeStart = new DateTime(2016, 1, 1, 11, 0, 0);
-            classB.TimeEnd = new DateTime(2016, 1, 1, 12, 45, 0);
-            classB.StartDate = DateTime.Now;
-            classB.EndDate = DateTime.Now.AddDays(84);
-            classB.ClassDays = "TR";
-            dbConnector.SaveClass(classB);
+            DateTime startTime2 = new DateTime(2016, 1, 1, 11, 0, 0);
+            DateTime endTime2 = new DateTime(2016, 1, 1, 12, 45, 0);
+            dbConnector.CreateClass("CSCI 1101", "B", "Math Structures", 3,
+                "AH 300", "Grayson", startTime, endTime, DateTime.Now, DateTime.Now.AddDays(84), "TR");
 
-            Class classC = new Class();
-            classC.CourseNo = "MATH 2011";
-            classC.Section = "D";
-            classC.ClassName = "Calculus";
-            classC.Credits = 4;
-            classC.Location = "AH 200";
-            classC.Instructor = "Pennyworth";
-            classC.TimeStart = new DateTime(2016, 1, 1, 2, 0, 0);
-            classC.TimeEnd = new DateTime(2016, 1, 1, 3, 45, 0);
-            classC.StartDate = DateTime.Now;
-            classC.EndDate = DateTime.Now.AddDays(84);
-            classC.ClassDays = "TR";
-            dbConnector.SaveClass(classC);
+            DateTime startTime3 = new DateTime(2016, 1, 1, 2, 0, 0);
+            DateTime endTime3 = new DateTime(2016, 1, 1, 3, 45, 0);
+            dbConnector.CreateClass("MATH 2011", "D", "Calculus", 4,
+                "AH 200", "Pennyworth", startTime, endTime, DateTime.Now, DateTime.Now.AddDays(84), "TR");
         }
     }
 }
