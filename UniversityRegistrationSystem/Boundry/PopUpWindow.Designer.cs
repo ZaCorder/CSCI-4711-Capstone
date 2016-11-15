17f8e31d7d7236ea2542bf017e7d8ab338e79d1f
Jonathan Delaigle <JDELAIG1@augusta.edu>
2016-11-14 19:49:29 -0500

    Swapping in custom PopUpWindow form instead of Using MessageBox so that we can fulfil our class diagrams.

---
+++ UniversityRegistrationSystem/Boundry/PopUpWindow.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace UniversityRegistrationSystem.Boundry
+{
+    partial class PopUpWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.Location = new System.Drawing.Point(12, 9);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(259, 52);
+            this.messageLabel.TabIndex = 0;
+            this.messageLabel.Text = "messageLabel";
+            this.messageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(196, 73);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 1;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // PopUpWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 108);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.messageLabel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PopUpWindow";
+            this.ShowIcon = false;
+            this.Text = "PopUpWindow";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.Button okButton;
+    }
+}
--- UniversityRegistrationSystem/Boundry/PopUpWindow.cs
+++ UniversityRegistrationSystem/Boundry/PopUpWindow.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,19 +10,24 @@ using System.Windows.Forms;
 
 namespace UniversityRegistrationSystem.Boundry
 {
-    /// <summary>
-    /// Display a pop up window.
-    /// </summary>
-    public static class PopUpWindow
+    public partial class PopUpWindow : Form
     {
-        /// <summary>
-        /// Display a popup window the specified message and title.
-        /// </summary>
-        /// <param name="message">The message to be displayed to the user.</param>
-        /// <param name="title">The text to be displayed as the title.</param>
-        public static void Display(string message, string title ="")
+        public PopUpWindow()
         {
-            MessageBox.Show(message, title, MessageBoxButtons.OK);
+            InitializeComponent();
+        }
+
+        public static void Display(string message, string title = "")
+        {
+            PopUpWindow popup = new PopUpWindow();
+            popup.messageLabel.Text = message;
+            popup.Text = title;
+            popup.ShowDialog();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
