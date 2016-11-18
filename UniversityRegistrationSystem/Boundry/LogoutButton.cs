809b8b82dbf9aa161cc6ccb95cb999c713b98f36
Jonathan Delaigle <JDELAIG1@augusta.edu>
2016-11-17 20:55:32 -0500

    First pass at adding logout

---
+++ UniversityRegistrationSystem/Boundry/LogoutButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversityRegistrationSystem.Boundry
+{
+    public partial class LogoutButton : Form
+    {
+        private EventHandler logoutListener;
+
+        public LogoutButton(EventHandler logoutListener)
+        {
+            this.logoutListener = logoutListener;
+            this.SetTopLevel(false);
+            InitializeComponent();
+        }
+
+        private void logOut_Click(object sender, EventArgs e)
+        {
+            this.logoutListener.Invoke(sender, e);
+        }
+    }
+}
