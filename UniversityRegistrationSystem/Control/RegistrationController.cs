5f45dac92efbb7d74a67e729db721528ec83c406
zane-lt\zane <zcorder@augusta.edu>
2016-11-19 12:31:25 -0500

    Adding preliminary controller for Registration.

---
+++ UniversityRegistrationSystem/Control/RegistrationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityRegistrationSystem.Control
+{
+    class RegistrationController : Controller
+    {
+        private DBConnect db;
+
+        public RegistrationController(DBConnect db) : base(db)
+        {
+            this.db = db;
+        }
+
+        public Student Register(string fullCourseId)
+        {
+
+            Account user = GetLoggedInUser();
+            return this.Register(fullCourseId, user);
+        }
+    }
+}
