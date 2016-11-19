abf186d1ecbd3dc01e907053bd8f4183ce836868
michael_townsend <michael.h.townsend@gmail.com>
2016-11-19 11:27:30 -0500

    Updated LoginForm design

--- UniversityRegistrationSystem/Form1.cs
+++ UniversityRegistrationSystem/Form1.cs
@@ -16,5 +16,15 @@ namespace UniversityRegistrationSystem
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
