79aa784ce71aa7bdb4bc8c98ca8e99e98c9f3d1f
michael_townsend <michael.h.townsend@gmail.com>
2016-11-19 11:38:18 -0500

    more changes to loginForm design

--- UniversityRegistrationSystem/Form1.Designer.cs
+++ UniversityRegistrationSystem/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -46,7 +47,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(114, 197);
+            this.textBox2.Location = new System.Drawing.Point(114, 173);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(273, 22);
             this.textBox2.TabIndex = 1;
@@ -63,7 +64,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(35, 202);
+            this.label2.Location = new System.Drawing.Point(35, 173);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(73, 17);
             this.label2.TabIndex = 3;
@@ -87,11 +88,22 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "Please enter your Email address and Password to login in.";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(177, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(404, 253);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -114,6 +126,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- UniversityRegistrationSystem/Form1.cs
+++ UniversityRegistrationSystem/Form1.cs
@@ -26,5 +26,10 @@ namespace UniversityRegistrationSystem
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
