c97b90b7074a7cc8e38c8af76feaa0784675f6d8
michael_townsend <michael.h.townsend@gmail.com>
2016-11-20 15:04:38 -0500

    Initial draft. Labels and drop-downs in place...no functionality yet or validation

---
+++ UniversityRegistrationSystem/Boundry/CreateClassWindow.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace UniversityRegistrationSystem.Boundry
+{
+    partial class CreateClassWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateClasslbl = new System.Windows.Forms.Label();
+            this.CourseNolbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.StartDate = new System.Windows.Forms.DateTimePicker();
+            this.EndDate = new System.Windows.Forms.DateTimePicker();
+            this.EndTime = new System.Windows.Forms.DateTimePicker();
+            this.StartTime = new System.Windows.Forms.DateTimePicker();
+            this.DayOfClass = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // CreateClasslbl
+            // 
+            this.CreateClasslbl.AutoSize = true;
+            this.CreateClasslbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateClasslbl.Location = new System.Drawing.Point(57, 33);
+            this.CreateClasslbl.Name = "CreateClasslbl";
+            this.CreateClasslbl.Size = new System.Drawing.Size(420, 54);
+            this.CreateClasslbl.TabIndex = 0;
+            this.CreateClasslbl.Text = "Create a new class";
+            // 
+            // CourseNolbl
+            // 
+            this.CourseNolbl.AutoSize = true;
+            this.CourseNolbl.Location = new System.Drawing.Point(35, 121);
+            this.CourseNolbl.Name = "CourseNolbl";
+            this.CourseNolbl.Size = new System.Drawing.Size(109, 17);
+            this.CourseNolbl.TabIndex = 1;
+            this.CourseNolbl.Text = "Course number:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 366);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Instructor:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 318);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Location:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Credits:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Class name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(85, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Section:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(151, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H"});
+            this.comboBox1.Location = new System.Drawing.Point(151, 172);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox2.Location = new System.Drawing.Point(151, 265);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(151, 226);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 22);
+            this.textBox2.TabIndex = 11;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Allgood Hall",
+            "University Hall",
+            "Science Building"});
+            this.comboBox3.Location = new System.Drawing.Point(151, 309);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 12;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Mike Dowell",
+            "Onyeka Ezenwoye",
+            "Harley Eades",
+            "Paul York",
+            "Todd Schultz"});
+            this.comboBox4.Location = new System.Drawing.Point(151, 357);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 24);
+            this.comboBox4.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(385, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Start time:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(388, 283);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "End date:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(383, 229);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Start date:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(413, 332);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 17);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Days:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(390, 179);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(67, 17);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "End time:";
+            // 
+            // StartDate
+            // 
+            this.StartDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.StartDate.Location = new System.Drawing.Point(464, 229);
+            this.StartDate.Name = "StartDate";
+            this.StartDate.Size = new System.Drawing.Size(200, 22);
+            this.StartDate.TabIndex = 21;
+            // 
+            // EndDate
+            // 
+            this.EndDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.EndDate.Location = new System.Drawing.Point(464, 283);
+            this.EndDate.Name = "EndDate";
+            this.EndDate.Size = new System.Drawing.Size(200, 22);
+            this.EndDate.TabIndex = 22;
+            // 
+            // EndTime
+            // 
+            this.EndTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.EndTime.Location = new System.Drawing.Point(464, 179);
+            this.EndTime.Name = "EndTime";
+            this.EndTime.Size = new System.Drawing.Size(200, 22);
+            this.EndTime.TabIndex = 23;
+            // 
+            // StartTime
+            // 
+            this.StartTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.StartTime.Location = new System.Drawing.Point(464, 126);
+            this.StartTime.Name = "StartTime";
+            this.StartTime.Size = new System.Drawing.Size(200, 22);
+            this.StartTime.TabIndex = 24;
+            // 
+            // DayOfClass
+            // 
+            this.DayOfClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DayOfClass.FormattingEnabled = true;
+            this.DayOfClass.Items.AddRange(new object[] {
+            "M/W/F",
+            "T/Tr"});
+            this.DayOfClass.Location = new System.Drawing.Point(464, 332);
+            this.DayOfClass.Name = "DayOfClass";
+            this.DayOfClass.Size = new System.Drawing.Size(121, 24);
+            this.DayOfClass.TabIndex = 25;
+            // 
+            // CreateClassWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1449, 550);
+            this.Controls.Add(this.DayOfClass);
+            this.Controls.Add(this.StartTime);
+            this.Controls.Add(this.EndTime);
+            this.Controls.Add(this.EndDate);
+            this.Controls.Add(this.StartDate);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CourseNolbl);
+            this.Controls.Add(this.CreateClasslbl);
+            this.Name = "CreateClassWindow";
+            this.Text = "CreateClassWindow";
+            this.Load += new System.EventHandler(this.CreateClassWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CreateClasslbl;
+        private System.Windows.Forms.Label CourseNolbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DateTimePicker StartDate;
+        private System.Windows.Forms.DateTimePicker EndDate;
+        private System.Windows.Forms.DateTimePicker EndTime;
+        private System.Windows.Forms.DateTimePicker StartTime;
+        private System.Windows.Forms.ComboBox DayOfClass;
+    }
+}
---
+++ UniversityRegistrationSystem/Boundry/CreateClassWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversityRegistrationSystem.Boundry
+{
+    public partial class CreateClassWindow : Form
+    {
+        public CreateClassWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateClassWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
